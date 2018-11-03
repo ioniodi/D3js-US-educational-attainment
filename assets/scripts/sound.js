fd71c1f4017ae4ed830c9fab35cab74aa4c734ce
Spyrivasi <43912901+Spyrivasi@users.noreply.github.com>
2018-11-03 15:40:56 +0200

    Update sound.js

--- assets/scripts/sound.js
+++ assets/scripts/sound.js
@@ -1,4 +1,7 @@
 // Javascript for playing a sound
+<audio controls preload="auto">
+	<source src="assets/data/Error-sound.mp3" controls></source>
+</audio>
 
 var beepOne = $("#beep-one")[0];
 $("#nav-one a")
