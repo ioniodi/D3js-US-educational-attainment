8ee70c3e16b5332d8d00e83f770ab094f17e8e6e
giottachou <43947889+giottachou@users.noreply.github.com>
2018-11-06 17:51:24 +0200

    Update script_1.js

--- assets/scripts/script_1.js
+++ assets/scripts/script_1.js
@@ -20,7 +20,7 @@ function donutChart() {
     var width,
         height,
         margin = {top: 10, right: 10, bottom: 10, left: 10},
-        colour = d3.scaleOrdinal().range(["#0FA3B1", "#73c3bf", "#a1d8c8", "#cbe0a7", "#f2db84", "#f7af72", "#FF9B42"]), // colour scheme
+        colour = d3.scaleOrdinal().range(["#AC3579", "#A26082", "#BDA59A", "#B4FBD3", "#E2FB3E", "#FFC5FF", "#FFD000"]), // colour scheme
         variable, // value in data that will dictate proportions on chart
         category, // compare data by
         padAngle, // effectively dictates the gap between slices
