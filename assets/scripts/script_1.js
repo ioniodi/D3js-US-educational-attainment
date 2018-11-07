b9d823c0ce2e84fe4ce54bd5b779dd7b989b8e54
elenhspinou <32733998+elenhspinou@users.noreply.github.com>
2018-11-07 20:06:48 +0200

    Update script_1.js

--- assets/scripts/script_1.js
+++ assets/scripts/script_1.js
@@ -20,7 +20,7 @@ function donutChart() {
     var width,
         height,
         margin = {top: 10, right: 10, bottom: 10, left: 10},
-+        colour = d3.scaleOrdinal().range(["#ff1a1a", "#ffcccc", "ff9999", "#ff6666", "#990000", "#660000", "#b30000"]),
+        colour = d3.scaleOrdinal().range(["#ff1a1a", "#ffcccc", "ff9999", "#ff6666", "#990000", "#660000", "#b30000"]),
 // #0FA3B1", "#73c3bf", "#a1d8c8", "#cbe0a7", "#f2db84", "#f7af72", "#FF9B42"
  // colour scheme
         variable, // value in data that will dictate proportions on chart
