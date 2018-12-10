2bf71eb2805ae18319b6ac2d7e1f6eebd9189335
Sofronia Foutsitzi P2017063 <44113197+fsofronia@users.noreply.github.com>
2018-12-10 18:17:14 +0200

    Update script_1.js

--- assets/scripts/script_1.js
+++ assets/scripts/script_1.js
@@ -5,6 +5,7 @@ var donut = donutChart()
         .height(450)
         .cornerRadius(0.02) // sets how rounded the corners are on each slice
         .padAngle(0.005) // effectively dictates the gap between slice
+        .innerRadius(0.00)
         .variable('Percent')
         .category('Education Level');
 
@@ -12,8 +13,8 @@ var donut = donutChart()
         if (error) throw error;
         d3.select('#pie-chart')
             .datum(data) // bind data to the div
-            .call(donut) // draw chart in div
-            .innerRadius(0.00);
+            .call(donut); // draw chart in div
+            
           
             
     });
