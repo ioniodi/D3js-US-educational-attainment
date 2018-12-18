b81a264a75df64c0b474abe82057774a347c2e8d
Sofronia Foutsitzi P2017063 <44113197+fsofronia@users.noreply.github.com>
2018-12-18 04:37:01 +0200

    Update script_1.js

--- assets/scripts/script_1.js
+++ assets/scripts/script_1.js
@@ -4,7 +4,7 @@ var donut = donutChart()
         .width(960)
         .height(450)
         .cornerRadius(30) // sets how rounded the corners are on each slice
-        .padAngle(10) // effectively dictates the gap between slice
+        .padAngle(350) // effectively dictates the gap between slice
         .variable('Percent')
         .category('Education Level');
 
